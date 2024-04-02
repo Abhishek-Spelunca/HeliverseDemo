b5b06c1a05adc518e57302037bb3e067676ebc91
Abhishek S <abxishek19@gmail.com>
2024-04-02 14:40:02 +0530

    Commit101

---
+++ app/src/main/java/com/example/heliverse/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.heliverse;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
